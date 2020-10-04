88982da57d47cf1e6424c5db28bd281b56294784
Stanislav Uschakow <stanislav.uschakow@gmail.com>
2020-10-04 22:00:06 +0200

    Added sandbox for the website with a simple node backend to test it

---
+++ esp/configUI/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(express.static('./'));
+
+var server = app.listen(8080, function() {
+    
+});
+
+app.get('/', function(req, res) {res.sendFile('./index.html')});
+app.get('/config', function(req, res) {res.sendFile('config.html')});
+app.get('/style_index.css', function(req, res) {res.sendFile('style_index.css')});
+app.get('/style_config.css', function(req, res) {res.sendFile('style_config.css')});
