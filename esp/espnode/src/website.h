666bb186810cdc57247c74826cde94138be67a3e
Stanislav Uschakow <stanislav.uschakow@gmail.com>
2020-10-04 21:56:27 +0200

    Added POC for the website

---
+++ esp/espnode/src/website.h
@@ -0,0 +1,20 @@
+#include <string>
+
+const std::string website = R"(
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Countdown Timer</title>
+        <!-- <link rel="stylesheet" href="css/style.css" /> -->
+        <!-- <script src="js/script.js" defer></script> -->
+    </head>
+
+<body>
+<h1> Welcome to the ESP8266 </h1>
+
+</body>
+
+</html>
+)";
