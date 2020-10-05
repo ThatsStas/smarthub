f32d5866385dadb55456472c87c4110189a15480
Stanislav Uschakow <stanislav.uschakow@gmail.com>
2020-10-05 18:15:08 +0200

    Added backend api and files for the config and index js files

--- esp/configUI/app.js
+++ esp/configUI/app.js
@@ -10,3 +10,5 @@ app.get('/', function(req, res) {res.sendFile('./index.html')});
 app.get('/config', function(req, res) {res.sendFile('config.html')});
 app.get('/style_index.css', function(req, res) {res.sendFile('style_index.css')});
 app.get('/style_config.css', function(req, res) {res.sendFile('style_config.css')});
+app.get('/config.js', function(req, res) {res.sendFile('config.js')});
+app.get('/indes.js', function(req, res) {res.sendFile('index.js')});
