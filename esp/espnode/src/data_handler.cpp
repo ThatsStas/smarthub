8240bdbf0d63ed39f344db3a135dfbace086b793
Stanislav Uschakow <stanislav.uschakow@gmail.com>
2020-12-27 13:12:54 +0100

    Added data handling class to the project. Not tested yet.

--- esp/espnode/src/data_handler.cpp
+++ esp/espnode/src/data_handler.cpp
@@ -3,14 +3,70 @@
 #include "EEPROM.h"
 
 DataHandler::DataHandler() {
-    EEPROM.begin(512);
-    configurationData = (dataStructure*)getData(512);
+    EEPROM.begin(MAX_EEPROM_SIZE);
+    EEPROM.get(BASE_ADDRESS, configurationData);
 }
 
-char* DataHandler::getBrokerAddress() {
-
+DataHandler::~DataHandler() {
+    EEPROM.end();
 }
 
-byte* DataHandler::getData(size_t numBytes) {
-    return nullptr;
+char* DataHandler::hostname(const char* hostname) {
+    Serial.println("II: Hostname");
+    return updateData(configurationData.hostname, hostname);
+}
+
+char* DataHandler::wifiSSID(const char* wifiSSID) {
+    Serial.println("II: WifiSSID");
+    return updateData(configurationData.wifiSSID, wifiSSID);
+}
+
+char* DataHandler::wifiPass(const char* wifiPass) {
+        Serial.println("II: WifiPassword");
+    return updateData(configurationData.wifiPass, wifiPass);
+}
+
+char* DataHandler::brokerAddress(const char* brokerAddress) {
+    Serial.println("II: BrokerAddress");
+    return updateData(configurationData.brokerAddress, brokerAddress);
+}
+
+char* DataHandler::brokerUser(const char* brokerUser) {
+    Serial.println("II: BrokerUser");
+    return updateData(configurationData.brokerUser, brokerUser);
+}
+
+char* DataHandler::brokerPassword(const char* brokerPassword) {
+    Serial.println("II: BrokerPassword");
+    return updateData(configurationData.brokerPassword, brokerPassword);
+}
+
+char* DataHandler::brokerTopic(const char* brokerTopic) {
+    Serial.println("II: BrokerTopic");
+    return updateData(configurationData.brokerTopic, brokerTopic);
+}
+
+char* DataHandler::updateData(char* originalValue, const char* updateValue)
+{
+    if (updateValue != nullptr && strlen(updateValue) < MAX_SIZE) 
+    {
+        Serial.println("DD: Updating value");
+        memcpy(originalValue, updateValue, strlen(updateValue));
+    }
+    else 
+    {   
+        Serial.println("EE: Data is too large. Returning old data.");
+    }
+    return originalValue;
+}
+
+uint32 DataHandler::updateInterval(const uint32 updateInterval) {
+    if (updateInterval != 0)
+        configurationData.updateInterval = updateInterval;
+    return configurationData.updateInterval;
+}
+
+bool DataHandler::writeData() {
+    EEPROM.put(BASE_ADDRESS, configurationData);
+    return EEPROM.commit();
 }
--- esp/espnode/src/data_handler.h
+++ esp/espnode/src/data_handler.h
@@ -7,26 +7,35 @@ class DataHandler {
 public:
     DataHandler();
     ~DataHandler();
-    bool writeData();
-    char* getBrokerAddress();
-    char* getBrokerUser();
+
+    char* hostname(const char* hostname = nullptr);
+    char* wifiSSID(const char* wifiSSID = nullptr);
+    char* wifiPass(const char* wifiPass = nullptr);
+    char* brokerAddress(const char* brokerAddress = nullptr);
+    char* brokerUser(const char* brokerUser = nullptr);
+    char* brokerPassword(const char* brokerPassword = nullptr);
+    char* brokerTopic(const char* brokerTopic = nullptr);
+    uint32 updateInterval(const uint32 updateInterval = 0);
 
 
 private:
+    static const uint16 MAX_SIZE = 64;
+    static const uint16 BASE_ADDRESS = 0;
+    static const uint16 MAX_EEPROM_SIZE = 512;
+
     struct dataStructure {
-        char* hostname;
-        char* wifiSSID;
-        char* wifiPass;
-        char* brokerAddress;
-        char* brokerUser;
-        char* brokerPassword;
-        char* brokerTopic;
+        char hostname[MAX_SIZE];
+        char wifiSSID[MAX_SIZE];
+        char wifiPass[MAX_SIZE];
+        char brokerAddress[MAX_SIZE];
+        char brokerUser[MAX_SIZE];
+        char brokerPassword[MAX_SIZE];
+        char brokerTopic[MAX_SIZE];
         uint32 updateInterval;
-    } *configurationData;
+    } configurationData;
 
-    
-
-    byte* getData(size_t numBytes);
+    char* updateData(char* originalValue, const char* updateValue);
+    bool writeData();
 };
 
 #endif
